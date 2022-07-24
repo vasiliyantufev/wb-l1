46f6c8569fd4f2b36ebc17a3abb78adf4a035e49
vasiliyantufev <vasiliyantufev@gmail.com>
2022-07-25 01:33:31 +0300

    4-5

--- 5/pr5.go
+++ 5/pr5.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
+/*
+Разработать программу, которая будет последовательно отправлять значения
+в канал, а с другой стороны канала — читать.
+По истечению N секунд программа должна завершаться.
+*/
+
 func main() {
+
+	ch := make(chan int)
+	go print(ch)
+	go send(ch)
+	//go print(ch)
+	time.Sleep(time.Duration(10 * time.Second))
+	close(ch)
+}
+
+func print(ch <-chan int) {
+	for v := range ch {
+		fmt.Println(v)
+	}
+}
+
+func send(ch chan<- int) {
+	i := 0
+	for {
+		ch <- i
+		i++
+	}
 
 }
