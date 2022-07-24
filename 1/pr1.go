13666bebe9f2695a116666a20f011df4081b56de
vasiliyantufev <vasiliyantufev@gmail.com>
2022-07-24 20:17:14 +0300

    task 1-4

--- 1/pr1.go
+++ 1/pr1.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import "fmt"
 
+/*
+task #1
+Дана структура Human (с произвольным набором полей и методов).
+Реализовать встраивание методов в структуре Action от родительской структуры Human (аналог наследования).
+*/
+
+type Human struct {
+	name string
+	age  int
+	sex  bool
+}
+
+func (h *Human) Hi() {
+	fmt.Println("Hi kitty!")
+}
+
+type Action struct {
+	Human
+}
+
+func main() {
+	Action := new(Action)
+	Action.Hi()
 }
--- 3/pr3.go
+++ 3/pr3.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+/*
+task #3
+Дана последовательность чисел: 2,4,6,8,10. Найти сумму их
+квадратов(22+32+42….) с использованием конкурентных вычислений.
+*/
+
+func main() {
+	array := []int{2, 4, 6, 8, 10}
+
+	result := 0
+
+	for _, a := range array {
+		result += a * a
+	}
+	fmt.Printf("Сумма квадратов = %v\n", result)
 }
